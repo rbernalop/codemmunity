34c2f86e74a140f62d6f08882a5b22d43ee168b2
rbernalop <rafabernabeu3@gmail.com>
2023-02-10 18:06:07 +0100

    Implemented application tests

---
+++ src/api-authentication/src/test/java/org/rbernalop/apiauthentication/application/create/UserMother.java
@@ -0,0 +1,19 @@
+package org.rbernalop.apiauthentication.application.create;
+
+import org.rbernalop.apiauthentication.domain.aggregate.User;
+import org.rbernalop.apiauthentication.domain.value_object.*;
+
+public class UserMother {
+
+    public static User fromCommand(CreateUserCommand command) {
+        return new User(
+            new UserId(command.getId()),
+            new UserName(command.getName()),
+            new UserSurname(command.getSurname()),
+            new UserUsername(command.getUsername()),
+            new UserEmail(command.getEmail()),
+            new UserPassword(command.getPassword()),
+            new UserBirthDate(command.getBirthDate())
+        );
+    }
+}
