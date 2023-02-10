8ec5e53c5f5c7d6988996b475e1272d3d007e28b
rbernalop <rafabernabeu3@gmail.com>
2023-02-10 16:01:51 +0100

    Configured API Authentication

--- src/api-authentication/src/main/java/org/rbernalop/apiauthentication/APIAuthenticationApplication.java
+++ src/api-authentication/src/main/java/org/rbernalop/apiauthentication/APIAuthenticationApplication.java
@@ -3,9 +3,11 @@ package org.rbernalop.apiauthentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan({"org.rbernalop.shared", "org.rbernalop.apiauthentication"})
 public class APIAuthenticationApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIAuthenticationApplication.class, args);
