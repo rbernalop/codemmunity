3c32f8377a7db5cc60ea0ab41ba574a036f3eab8
rbernalop <rafabernabeu3@gmail.com>
2023-02-11 15:20:04 +0100

    Fixed exception handling

--- src/api-authentication/src/main/java/org/rbernalop/apiauthentication/infrastructure/controller/CustomExceptionHandler.java
+++ src/api-authentication/src/main/java/org/rbernalop/apiauthentication/infrastructure/controller/CustomExceptionHandler.java
@@ -5,15 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 import org.rbernalop.apiauthentication.domain.exception.InvalidUserDataException;
 import org.rbernalop.apiauthentication.domain.exception.UserAlreadyExistsException;
+import org.rbernalop.shared.domain.InvalidIdException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
+@RestController
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     private CustomError generateError(String message, HttpStatus httpStatus) {
         return CustomError.builder()
@@ -24,15 +27,15 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(InvalidUserDataException.class)
-    public CustomError handleInvalidUserDataExceptionException(InvalidUserDataException ex) {
-        return generateError(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler({InvalidUserDataException.class, InvalidIdException.class})
+    public CustomError handleInvalidUserDataExceptionException(Exception ex) {
+        return generateError(ex.getCause().getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public CustomError handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
-        return generateError(ex.getMessage(), HttpStatus.CONFLICT);
+    public CustomError handleUserAlreadyExistsException(Exception ex) {
+        return generateError(ex.getCause().getMessage(), HttpStatus.CONFLICT);
     }
 }
 
