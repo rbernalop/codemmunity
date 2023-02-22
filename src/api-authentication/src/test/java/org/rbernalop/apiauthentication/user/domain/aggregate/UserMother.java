9444eec7dee2c07a3430e6791021bb0d0251b457
rbernalop <rafabernabeu3@gmail.com>
2023-02-22 19:33:17 +0100

    Implemmented and tested login controller

--- src/api-authentication/src/test/java/org/rbernalop/apiauthentication/user/domain/aggregate/UserMother.java
+++ src/api-authentication/src/test/java/org/rbernalop/apiauthentication/user/domain/aggregate/UserMother.java
@@ -1,11 +1,15 @@
 package org.rbernalop.apiauthentication.user.domain.aggregate;
 
+import com.github.javafaker.Faker;
 import org.rbernalop.apiauthentication.shared.application.user.create.CreateUserCommand;
 import org.rbernalop.apiauthentication.shared.application.user.create.CreateUserCommandMother;
 import org.rbernalop.apiauthentication.user.domain.value_object.*;
 
+import java.time.ZoneId;
+
 public class UserMother {
 
+    private final static Faker faker = new Faker();
     public static User fromCommand(CreateUserCommand command) {
         return new User(
             new UserId(command.getId()),
@@ -20,5 +24,17 @@ public class UserMother {
 
     public static User random() {
         return fromCommand(CreateUserCommandMother.random());
+    }
+
+    public static User fromCredentials(String username, String password) {
+        return new User(
+            new UserId(faker.internet().uuid()),
+            new UserName(faker.name().firstName()),
+            new UserSurname(faker.name().lastName()),
+            new UserUsername(username),
+            new UserEmail(faker.internet().emailAddress()),
+            new UserPasswordEncrypted(password),
+            new UserBirthDate(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+        );
     }
 }
