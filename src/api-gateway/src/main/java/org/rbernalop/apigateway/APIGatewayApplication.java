1ae317d88222b8bc7c17f3b25b6933f0b8a663f0
rbernalop <rafabernabeu3@gmail.com>
2023-03-04 20:15:27 +0100

    Fixed microservices security configuration

--- src/api-gateway/src/main/java/org/rbernalop/apigateway/APIGatewayApplication.java
+++ src/api-gateway/src/main/java/org/rbernalop/apigateway/APIGatewayApplication.java
@@ -1,12 +1,16 @@
 package org.rbernalop.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = {"org.rbernalop.apigateway", "org.rbernalop.shared"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class, ReactiveManagementWebSecurityAutoConfiguration.class})
+@ComponentScan({"org.rbernalop.shared", "org.rbernalop.apigateway"})
 public class APIGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIGatewayApplication.class, args);
