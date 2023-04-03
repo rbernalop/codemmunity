ed2840869792f10f98eb2bf8a7c5968c80781dc2
rbernalop <rafabernabeu3@gmail.com>
2023-04-03 11:25:39 +0200

    Created seeders

---
+++ src/api-execution/src/main/resources/seeders.sql
@@ -0,0 +1,6 @@
+DELETE FROM executiondb.public.languages;
+
+INSERT INTO executiondb.public.languages (language_id, created_at, updated_at, name) VALUES
+('d318ab48-ea92-4c3d-95b2-1df9cbbf52eb', '2022-04-01 12:23:45', '2022-04-01 12:23:45', 'Python'),
+('d22b17a8-277e-40c7-819a-2f1c9c68da67', '2022-04-02 08:15:30', '2022-04-02 08:15:30', 'Java'),
+('d5ee2b6e-cb52-4b6e-82f4-46f4b4e9e0d1', '2022-04-03 11:05:20', '2022-04-03 11:05:20', 'NodeJS');
