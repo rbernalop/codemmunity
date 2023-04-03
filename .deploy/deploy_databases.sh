bac43260229dcbc7fe4a2e077881e2c87f7a1aff
rbernalop <rafabernabeu3@gmail.com>
2023-04-03 11:39:14 +0200

    Changed deploy database command

--- .deploy/deploy_databases.sh
+++ .deploy/deploy_databases.sh
@@ -1,4 +1,4 @@
 docker compose up applicationdb -d
-docker exec -it applicationdb psql -U postgres -c "CREATE DATABASE authenticationdb;"
-docker exec -it applicationdb psql -U postgres -c "CREATE DATABASE scriptdb;"
-docker exec -it applicationdb psql -U postgres -c "CREATE DATABASE executiondb;"
+docker exec -T applicationdb psql -U postgres -c "CREATE DATABASE authenticationdb;"
+docker exec -T applicationdb psql -U postgres -c "CREATE DATABASE scriptdb;"
+docker exec -T applicationdb psql -U postgres -c "CREATE DATABASE executiondb;"
