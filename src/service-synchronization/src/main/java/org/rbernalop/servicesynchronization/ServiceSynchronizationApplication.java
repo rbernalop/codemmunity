23436c7fcd43481b018a66b0e35224486893c6ca
rbernalop <rafabernabeu3@gmail.com>
2023-04-04 17:51:59 +0200

    Created Service Synchronization base application

---
+++ src/service-synchronization/src/main/java/org/rbernalop/servicesynchronization/ServiceSynchronizationApplication.java
@@ -0,0 +1,18 @@
+package org.rbernalop.servicesynchronization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ComponentScan({"org.rbernalop.shared", "org.rbernalop.servicesynchronization"})
+public class ServiceSynchronizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSynchronizationApplication.class, args);
+    }
+}
