6c5836fb4092378b7c3a74125ed1ecfbc0f17082
rbernalop <rafabernabeu3@gmail.com>
2023-04-19 20:17:20 +0200

    Implemented and tested Challenge get by ID use case

--- src/api-challenge/src/main/java/org/rbernalop/apichallenge/challenge/application/finder/ChallengeFinder.java
+++ src/api-challenge/src/main/java/org/rbernalop/apichallenge/challenge/application/finder/ChallengeFinder.java
@@ -2,20 +2,24 @@ package org.rbernalop.apichallenge.challenge.application.finder;
 
 import org.rbernalop.apichallenge.challenge.domain.aggregate.Challenge;
 import org.rbernalop.apichallenge.challenge.domain.port.ChallengeRepository;
+import org.rbernalop.apichallenge.challenge.domain.value_object.ChallengeId;
 import org.rbernalop.apichallenge.shared.application.challenge.finder.all.FindChallengesPaginatedResponse;
+import org.rbernalop.apichallenge.shared.application.challenge.finder.by_id.FindChallengeByIdResponse;
+import org.rbernalop.shared.application.UseCase;
+import org.rbernalop.shared.domain.bus.event.EventBus;
 import org.rbernalop.shared.domain.bus.query.QueryBus;
+import org.rbernalop.shared.domain.exception.EntityNotFoundException;
 import org.rbernalop.shared.domain.valueobject.PaginationRequest;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
-public class ChallengeFinder {
+public class ChallengeFinder extends UseCase {
     private final ChallengeRepository challengeRepository;
-    private final QueryBus queryBus;
 
-    public ChallengeFinder(ChallengeRepository challengeRepository, QueryBus queryBus) {
+    public ChallengeFinder(QueryBus queryBus, EventBus eventBus, ChallengeRepository challengeRepository) {
+        super(queryBus, eventBus);
         this.challengeRepository = challengeRepository;
-        this.queryBus = queryBus;
     }
 
     public FindChallengesPaginatedResponse findChallengesPaginated(PaginationRequest paginationRequest) {
@@ -24,5 +28,12 @@ public class ChallengeFinder {
             PageRequest.of(paginationRequest.page(), paginationRequest.size())
         ).getContent();
         return FindChallengesPaginatedResponse.from(challenges, challengeRepository.count());
+    }
+
+    public FindChallengeByIdResponse findById(ChallengeId challengeId) {
+        Challenge challenge = challengeRepository.findById(challengeId).orElseThrow(() ->
+            new EntityNotFoundException("Challenge not found")
+        );
+        return FindChallengeByIdResponse.from(challenge);
     }
 }
